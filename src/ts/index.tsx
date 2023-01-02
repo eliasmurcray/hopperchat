55c38a9811b32174b61f0f02d4f94e82017034f1
eliasmurcray <eliasmurcray@gmail.com>
2023-01-01 17:38:20 -0800

    Update index.tsx to remove Formik dependency

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,70 +3,114 @@ import ReactDOM from "react-dom/client";
 import firebaseConfig from "../firebaseconfig.json";
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
-import { Formik, Form, Field, ErrorMessage } from "formik";
 import "../css/index.css";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Create the new user with email and password
-async function signupWithEmail(email: string, password: string): Promise<any> {
-  localStorage.setItem("email", email);
-  const { user } = await createUserWithEmailAndPassword(auth, email, password);
-  window["user"] = user;
-  return await sendEmailVerification(user, {
-    url: window.location.origin + "/onboarding"
-  });
-}
+class MyForm extends React.Component<{}, { [key:string]: any }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {
+        "email": "",
+        "new-password": "" 
+      }
+    };
+    this.onInput = this.onInput.bind(this);
+  }
 
-// Form signup component
-const EmailPasswordForm = (props: { onSuccess: () => void; }) => {
-  return (<Formik
-    initialValues={{ email: "", "new-password": ""}}
+  private async signupWithEmail(email: string, password: string): Promise<any> {
+    localStorage.setItem("email", email);
+    const { user } = await createUserWithEmailAndPassword(auth, email, password);
+    window["user"] = user;
+    return await sendEmailVerification(user, {
+      url: window.location.origin + "/onboarding"
+    });
+  }
 
-    validate={(values) => {
-      const errors = {};
-      if (values.email.length !== 0 && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) errors["email"] = "Invalid format";
-      if (values["new-password"].length !== 0 && values["new-password"].length < 8) errors["new-password"] = "Password must be at least 8 characters";
-      return errors;
-    }}
+  onInput(event: { target: any; }) {
+    const element = event.target;
+    const name = element.name;
+    const value = element.value;
+    this.setState({ errors: {} });
+    switch(name) {
+      case "email":
+        if(value.length !== 0 && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value))
+          this.setState({ errors: { "email": "Invalid format" }});
+        break;
+      case "new-password":
+        if(value.length !== 0 && value.length < 8)
+          this.setState({ errors: { "new-password": "Password must be at least 8 characters" }});
+    }
+  }
 
-    onSubmit={(values, { setSubmitting, setErrors }) => {
-      const { email, "new-password": password } = values;
-      setSubmitting(true);
-      signupWithEmail(email, password)
-      .then(() => {
-        setSubmitting(false);
-        props.onSuccess();
-      })
-      .catch((error) => {
-        switch(error.code) {
-          case "auth/email-already-in-use":
-            setErrors({ email: "Email already in use." });
-            break;
-          default:
-            setErrors({ email: "", "new-password": error });
-        }
-        setSubmitting(false);
-      });
-    }}>
-    {({isSubmitting, errors}) => (
-      <Form autoComplete="off">
-        <div className={"input-container"+(errors["email"]?" error":"")}>
-          <Field type="email" name="email" spellcheck="false" placeholder=" " pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required />
-          <label htmlFor="email">Email</label>
-        </div>
-        <ErrorMessage name="email" component="div" className="error-message"/>
-        <div className={"input-container"+(errors["new-password"]?" error":"")}>
-          <Field type="password" name="new-password" autocomplete="new-password" pattern="^.{8,}$" placeholder=" " required />
-          <label htmlFor="password">Password</label>
-        </div>
-        <ErrorMessage name="new-password" component="div" className="error-message" />
-        <button type="submit" disabled={isSubmitting}>Continue</button>
-      </Form>
-    )}
-  </Formik>);
+  render() {
+    return <form action="#" autoComplete="off">
+      <div className={`input-container${this.state.errors.email?" error":""}`}>
+        <input type="email" id="email" name="email" spellCheck="false" placeholder=" " pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" onInput={this.onInput} required />
+        <label htmlFor="email">Email</label>
+      </div>
+      <div className="error-message">{this.state.errors.email}</div>
+      <div className={`input-container${this.state.errors["new-password"]?" error":""}`}>
+        <input type="password" id="password" name="new-password" autoComplete="new-password" pattern="^.{8,}$" placeholder=" " onInput={this.onInput} required />
+        <label htmlFor="password">Password</label>
+      </div>
+      <div className="error-message">{this.state.errors["new-password"]}</div>
+      <button type="submit">Continue</button>
+    </form>
+  }
 };
+/*
+class EmailPasswordForm extends React.Component<{ onSuccess: () => void }> {
+
+  render() {
+    return (<Formik
+      initialValues={{ email: "", "new-password": ""}}
+  
+      validate={(values) => {
+        const errors = {};
+        if (values.email.length !== 0 && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) errors["email"] = "Invalid format";
+        if (values["new-password"].length !== 0 && values["new-password"].length < 8) errors["new-password"] = "Password must be at least 8 characters";
+        return errors;
+      }}
+  
+      onSubmit={(values, { setSubmitting, setErrors }) => {
+        const { email, "new-password": password } = values;
+        this.signupWithEmail(email, password)
+        .then(() => {
+          setSubmitting(false);
+          this.props.onSuccess();
+        })
+        .catch((error) => {
+          switch(error.code) {
+            case "auth/email-already-in-use":
+              setErrors({ email: "Email already in use." });
+              break;
+            default:
+              setErrors({ email: "", "new-password": error });
+          }
+          setSubmitting(false);
+        });
+      }}>
+      {({isSubmitting, errors}) => (
+        <Form autoComplete="off">
+          <div className={"input-container"+(errors["email"]?" error":"")}>
+            <Field type="email" id="email" name="email" spellcheck="false" placeholder=" " pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required />
+            <label htmlFor="email">Email</label>
+          </div>
+          <ErrorMessage name="email" component="div" className="error-message"/>
+          <div className={"input-container"+(errors["new-password"]?" error":"")}>
+            <Field type="password" id="password" name="new-password" autocomplete="new-password" pattern="^.{8,}$" placeholder=" " required />
+            <label htmlFor="password">Password</label>
+          </div>
+          <ErrorMessage name="new-password" component="div" className="error-message" />
+          <button type="submit" disabled={isSubmitting}>Continue</button>
+        </Form>
+      )}
+    </Formik>);
+  }
+};*/
 
 const EmailResendButton = () => {
   const [emailResent, setEmailResent] = React.useState(false);
@@ -100,7 +144,8 @@ const App = () => {
     <div className="step-container">
       <h1>Create Your Account</h1>
       <div className="small">Enter your email</div>
-      <EmailPasswordForm onSuccess={() => updateStep(0)}></EmailPasswordForm>
+      <MyForm></MyForm>
+      {/* <EmailPasswordForm onSuccess={() => updateStep(0)}></EmailPasswordForm> */}
       <div className="small">Have an account? <a href="/login">Log In</a></div>
     </div>,
     <div className="step-container">
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: "production",
@@ -32,6 +34,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
     clean: true
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new UglifyJsPlugin()
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
